2be0d2afc39e85fedfcb6fefb60e38754e53b480
Muhammad-Firzam <106610004+Muhammad-Firzam@users.noreply.github.com>
2022-06-14 21:09:13 +0500

    Update OOP_PRACTICE.java

--- src/oop_practice/OOP_PRACTICE.java
+++ src/oop_practice/OOP_PRACTICE.java
@@ -8,13 +8,13 @@ public class OOP_PRACTICE {
         c1.Area();
           Shape s1 = c1;
           s1.Area();
-//        s1.Parameters();
+        s1.Parameters();
         
-//        System.out.println("\n\t\t---Rectanlge---");
-//        Rectangle r1 = new Rectangle(3,4);
-//        Shape s2 = r1;
-//        s2.Area();
-//        s2.Parameters();
+        System.out.println("\n\t\t---Rectanlge---");
+        Rectangle r1 = new Rectangle(3,4);
+        Shape s2 = r1;
+        s2.Area();
+        s2.Parameters();
     }
     
 }
